14c47baae94b1b7d55b5c6a152a0e15c4b024dd6
jackhsu0225 <jackhsu0225@gmail.com>
2017-08-10 17:12:27 -0400

    test

--- EssentialTools/EssentialTools/Models/Product.cs
+++ EssentialTools/EssentialTools/Models/Product.cs
@@ -12,6 +12,7 @@ namespace EssentialTools.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+        public int test { get; set; }
 
     }
 }
