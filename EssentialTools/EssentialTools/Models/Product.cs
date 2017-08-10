60644704a89b9052e235a6c78ca143c67808313e
jackhsu0225 <jackhsu0225@gmail.com>
2017-08-10 17:21:00 -0400

    Delete test from Produc

--- EssentialTools/EssentialTools/Models/Product.cs
+++ EssentialTools/EssentialTools/Models/Product.cs
@@ -12,7 +12,6 @@ namespace EssentialTools.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
-        public int test { get; set; }
 
     }
 }
